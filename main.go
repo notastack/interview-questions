006d01a371a01b9ff229190f6537301c1d5d9e66
notastack <103752556+notastack@users.noreply.github.com>
2022-04-22 21:26:32 +0200

    Create main.go
    just a literal fizzbuzz

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	cfizz := 3
+	cbuzz := 5
+	for i := 1; i < 101; i++ {
+		switch {
+		case i%(cfizz * i%cbuzz) == 0:
+			fmt.Println("fizzbuzz")
+		case i%cfizz == 0:
+			fmt.Println("fizz")
+		case i%cbuzz == 0:
+			fmt.Println("buzz")
+		default:
+			fmt.Println(i)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+}
