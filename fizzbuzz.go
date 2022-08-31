42b3dfbcbff2a3960f2586070b5f0e5f9b243593
notastack <103752556+notastack@users.noreply.github.com>
2022-08-31 17:49:09 +0200

    Update fizzbuzz.go

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,42 +2,78 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	var cfizz, cbuzz, lower, higher int
+	var names []string
+	var values []int
+	var lower, higher int
 	//set up is made by the user himself
-	fmt.Println("What should the value of fizz be?")
-	fmt.Scanln(&cfizz)
-	fmt.Println("What should the value of buzz be?")
-	fmt.Scanln(&cbuzz)
+	names = getnames()
+	values = getvalues(names)
 	fmt.Println("Where should the counting start?")
 	fmt.Scanln(&lower)
 	fmt.Println("Where should the counting stop?")
 	fmt.Scanln(&higher)
 	//launch the fizzbuzz function
-	fibu(cfizz, cbuzz, lower, higher)
+	fibu(names, values, lower, higher)
 
 }
 
-func fibu(cfizz, cbuzz, lower, higher int) {
+func fibu(names []string, values []int, lower int, higher int) {
 	//loop from lower end to higher end
 	for i := lower; i < higher+1; i++ {
-		switch {
-		//if divisible by f and b then always divisible by f*b
-		case i%(cfizz*cbuzz) == 0:
-			fmt.Println("fizzbuzz")
-		case i%cfizz == 0:
-			fmt.Println("fizz")
-		case i%cbuzz == 0:
-			fmt.Println("buzz")
-		//error is default state so not divisible by f or b
-		default:
-			fmt.Println(i)
-		}
-		//give you time to read the answer
-		time.Sleep(1 * time.Second)
+		findinarray(i, values, names)
 	}
+
+}
+
+func findinarray(arrow int, target []int, name []string) {
+	var y int
+	var sentence []interface{}
+	y = len(name)
+	for i := 0; i < y; i++ {
+		if arrow%target[i] == 0 {
+			sentence = append(sentence, name[i])
+		}
+	}
+	if len(sentence) == 0 {
+		sentence = append(sentence, arrow)
+	}
+	fmt.Println(sentence)
+
+}
+
+func getnames() []string {
+	var namesgot []string
+	var i int
+	i = 1
+	var namewritten string
+	fmt.Println("writre [stop] when you entered all the names.")
+	for true {
+		fmt.Println("what should be the name of value number ", i, "?")
+		fmt.Scanln(&namewritten)
+		i++
+		if namewritten == "stop" {
+			return namesgot
+		} else {
+			namesgot = append(namesgot, namewritten)
+		}
+	}
+	return namesgot
+}
+
+func getvalues(x []string) []int {
+	var i int
+	var y []int
+	var valuegot int
+	i = 0
+	for i < len(x) {
+		fmt.Println("what should be the value of ", x[i], " to be?")
+		fmt.Scanln(&valuegot)
+		y = append(y, valuegot)
+		i++
+	}
+	return y
 
 }
