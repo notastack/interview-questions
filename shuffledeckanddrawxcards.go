caa1e674a7e7e88898799f920032625ab86b11d3
notastack <103752556+notastack@users.noreply.github.com>
2022-09-02 17:09:25 +0200

    Update shuffledeckanddrawxcards.go

--- shuffledeckanddrawxcards.go
+++ shuffledeckanddrawxcards.go
@@ -10,12 +10,13 @@ var drawn []int
 
 func main() {
 	var cfizz, z int
+	//user sets up variables
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("size of the deck?")
 	fmt.Scanln(&cfizz)
 	fmt.Println("how many cards to draw out?")
 	fmt.Scanln(&z)
-
+	
 	fibu(cfizz, z)
 
 }
@@ -23,6 +24,7 @@ func main() {
 func fibu(x, z int) {
 	var i int
 	var deck []int
+	// generate the deck
 	for i = 1; i < x+1; i++ {
 		deck = append(deck, i)
 	}
@@ -34,6 +36,7 @@ func fibu(x, z int) {
 }
 
 func areyouhere(s []int, str int) bool {
+	//check if the card's already been drawn
 	for _, v := range s {
 		if v == str {
 			return true
@@ -43,16 +46,20 @@ func areyouhere(s []int, str int) bool {
 }
 
 func draw(x []int) {
+	//select a random card
 	place = x[rand.Intn(len(x))]
+	//retry if already been drawn
 	if areyouhere(drawn, place) {
 		draw(x)
 	} else {
+		//add card to the list of cards already drawn and print it
 		drawn = append(drawn, place)
 		fmt.Println(place)
 	}
 }
 
 func suffle(x []int) []int{
+	//Fisher–Yates shuffle
 	for i := range x {
 		j := rand.Intn(i + 1)
 		x[i], x[j] = x[j], x[i]
