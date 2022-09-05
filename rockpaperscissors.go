b46fdcce13a221d587e1a79e692b4e198b247c47
notastack <103752556+notastack@users.noreply.github.com>
2022-09-05 14:06:28 +0200

    Update rockpaperscissors.go

--- rockpaperscissors.go
+++ rockpaperscissors.go
@@ -8,6 +8,38 @@ import (
 	"log"
 )
 
+func main() {
+	//the game
+	//also you lost
+	var usrplay int
+	// var cmtplay
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	 }
+	//introduce the player
+	username := user.Username
+	fmt.Printf("Hello %s\n", username)
+	fmt.Println("Lets play Rock Paper Scissors")
+	//you chose what you want to use
+	fmt.Println("Type 1 for Rock, 2 for Paper or 3 for Scissors")
+	fmt.Scanln(&usrplay)
+	fmt.Println("You choose")
+	choice(usrplay)
+	fmt.Println("The computer choose")
+	var cpuplay int
+	//generate a random choice for the cpu
+	rand.Seed(time.Now().UnixNano())
+	cpuplay = rand.Intn(3) + 1
+	// for testing purposes		
+	//cpuplay = 1
+	choice(cpuplay)
+	//output the result
+	result(usrplay, cpuplay)
+
+}
+
+
 func choice(v int) {
 	//convert the number to the RPS choice
 	choice := []string{"Rock", "Paper", "Scissors"}
@@ -20,7 +52,7 @@ func result(u int, c int) {
 		fmt.Println("It's a tie. Try again next time.")
 		return
 	}
-	//use a circular array to calculate the results because elif are ugly, take too much space and really slow
+	//Rotates the array to calculate the result
 	possible := []int{1, 2, 3}
 	var rot int
 	rot = u - 1
@@ -62,34 +94,4 @@ func rotateL(a []int, i int) {
 		}
 		a[j] = t
 	}
-}
-
-func main() {
-	//the game
-	//also you lost
-	var usrplay int
-	// var cmtplay
-	user, err := user.Current()
-	if err != nil {
-		log.Fatalf(err.Error())
-	 }
-	//introduce the player
-	username := user.Username
-	fmt.Printf("Hello %s\n", username)
-	fmt.Println("Lets play Rock Paper Scissors")
-	//you chose what you want to use
-	fmt.Println("Type 1 for Rock, 2 for Paper or 3 for Scissors")
-	fmt.Scanln(&usrplay)
-	fmt.Println("You choose")
-	choice(usrplay)
-	fmt.Println("The computer choose")
-	var cpuplay int
-	//generate a random choice for the cpu
-	rand.Seed(time.Now().UnixNano())
-	cpuplay = rand.Intn(3) + 1
-	// for testing purposes		cpuplay = 1
-	choice(cpuplay)
-	//output the result
-	result(usrplay, cpuplay)
-
 }
